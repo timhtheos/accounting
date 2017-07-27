1418c158ed711e04995ae22f4d4a9092f306287f
timhtheos <me@timothyae.com>
2017-07-27 10:27:45 +0800

    Update default controller - will not be used so far.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,13 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/default", name="homepage_default")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        /* return $this->render('default/index.html.twig', [ */
+        /*     'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR, */
+        /* ]); */
     }
 }
