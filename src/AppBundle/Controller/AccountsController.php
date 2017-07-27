ec28f8789d1f91a23518a1fc0814c16e05536eb0
timhtheos <me@timothyae.com>
2017-07-27 10:31:41 +0800

    Update Accounts controller.

--- src/AppBundle/Controller/AccountsController.php
+++ src/AppBundle/Controller/AccountsController.php
@@ -6,7 +6,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class TodoController extends Controller
+class AccountsController extends Controller
 {
-
+  /**
+   * @Route("/accounts", name="accounts")
+   */
+  public function indexAction()
+  {
+    return $this->render('accounts/index.html.twig');
+  }
 }
