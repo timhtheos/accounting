ba4ec1b220fbd35ebbbb61abc4b4c07545bede3b
timhtheos <me@timothyae.com>
2017-07-27 17:52:18 +0800

    Update accounts controller for add action.

--- src/AppBundle/Controller/AccountsController.php
+++ src/AppBundle/Controller/AccountsController.php
@@ -7,6 +7,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class AccountsController extends Controller
 {
   /**
@@ -20,6 +26,79 @@ class AccountsController extends Controller
 
     return $this->render('accounts/index.html.twig', array(
       'accounts' => $accounts
+    ));
+  }
+
+  /**
+   * @Route("/account/add", name="account_add")
+   */
+  public function addAction(Request $request)
+  {
+    $account = new Accounts;
+
+    $params = array('attr' => array(
+      'class' => 'form-control',
+      'style' => 'margin-bottom: 15px;'
+    ));
+
+    $params_submit = $params;
+    $params_submit['label'] = 'Add account';
+    $params_submit['attr']['class'] = 'btn btn-primary';
+
+    $params_type = $params;
+    $params_type['choices'] = array(
+      'Savings' => 'savings',
+      'Current' => 'current',
+      'Credit' => 'credit',
+      'Loan' => 'loan',
+    );
+
+    $params_currency = $params;
+    $params_currency['choices'] = array(
+      'PHP' => 'php',
+      'USD' => 'usd'
+    );
+
+    $form = $this->createFormBuilder($account)
+      ->add('name', TextType::class, $params)
+      ->add('type', ChoiceType::class, $params_type)
+      ->add('alias', TextType::class, $params)
+      ->add('currency', ChoiceType::class, $params_currency)
+      ->add('save', SubmitType::class, $params_submit)
+      ->getForm();
+
+    $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid()) {
+      // Get data.
+      $name = $form['name']->getData();
+      $type = $form['type']->getData();
+      $alias = $form['alias']->getData();
+      $currency = $form['currency']->getData();
+
+      $now = new\DateTime('now');
+
+      $account->setName($name);
+      $account->setType($type);
+      $account->setAlias($alias);
+      $account->setCurrency($currency);
+      $account->setDateAdded($now);
+
+      $em = $this->getDoctrine()->getManager();
+
+      $em->persist($account);
+      $em->flush();
+
+      $this->addFlash(
+        'notice',
+        'Account added.'
+      );
+
+      return $this->redirectToRoute('accounts');
+    }
+
+    return $this->render('accounts/add.html.twig', array(
+      'form' => $form->createView()
     ));
   }
 }
