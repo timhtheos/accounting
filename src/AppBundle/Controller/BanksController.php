d1d71d47c3863954a77c65215cb53c3d404f3fab
timhtheos <me@timothyae.com>
2017-07-28 19:24:20 +0800

    Update banks controller to list all banks.

--- src/AppBundle/Controller/BanksController.php
+++ src/AppBundle/Controller/BanksController.php
@@ -8,5 +8,17 @@ use Symfony\Component\HttpFoundation\Request;
 
 class BanksController extends Controller
 {
+  /**
+   * @Route("/banks", name="banks")
+   */
+  public function indexAction()
+  {
+    $banks = $this->getDoctrine()
+      ->getRepository('AppBundle:Banks')
+      ->findAll();
 
+    return $this->render('banks/index.html.twig', array(
+      'banks' => banks$
+    ));
+  }
 }
