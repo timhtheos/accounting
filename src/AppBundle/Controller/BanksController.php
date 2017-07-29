227fb7860f5d0a346a525ef571d848d4abf376fc
timhtheos <me@timothyae.com>
2017-07-29 11:29:39 +0800

    Update banks controller.

--- src/AppBundle/Controller/BanksController.php
+++ src/AppBundle/Controller/BanksController.php
@@ -41,6 +41,7 @@ class BanksController extends Controller
     ));
 
     $params_weight = $params;
+    $params_weight['label'] = 'Weight (optional)';
     $params_weight['choices'] = array(
       0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
       21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
@@ -53,8 +54,8 @@ class BanksController extends Controller
 
     $form = $this->createFormBuilder($bank)
       ->add('name', TextType::class, $params)
-      ->add('weight', ChoiceType::class, $params_weight)
       ->add('alias', TextType::class, $params)
+      ->add('weight', ChoiceType::class, $params_weight)
       ->add('save', SubmitType::class, $params_submit)
       ->getForm();
 
